452833f0681695a07376e001f20656dcec5c89b0
alisseo <alisseo@users.noreply.github.com>
2015-08-11 15:24:13 +0200

    Update block-ads-host-updater.bat

--- block-ads-host-updater.bat
+++ block-ads-host-updater.bat
@@ -1,17 +1,12 @@
 if exist HOSTS ( del HOSTS-bkp )
 copy /Y HOSTS HOSTS-bkp
-del hosts
+del HOSTS
 
 START /wait wget --referer="http://www.google.com" --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" --header="Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5" --header="Accept-Language: en-us,en;q=0.5" --header="Keep-Alive: 300" http://someonewhocares.org/hosts/hosts -O hostA.txt
 START /wait wget --referer="http://www.google.com" --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" --header="Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5" --header="Accept-Language: en-us,en;q=0.5" --header="Accept-Encoding: gzip,deflate" --header="Keep-Alive: 300" http://winhelp2002.mvps.org/hosts.txt -O hostB.txt.gz
-REM ## ""RIMOSSO PER ECCESSIVE RESTRIZIONI"" wget --referer="http://www.google.com" --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" --header="Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5" --header="Accept-Language: en-us,en;q=0.5" --header="Accept-Encoding: gzip,deflate" --header="Keep-Alive: 300" http://hosts.eladkarako.com/hosts.txt -O hostC.txt.gz
-
 START /wait wget --referer="http://www.google.com" --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6" "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&startdate[day]=&startdate[month]=&startdate[year]=&mimetype=plaintext" -O hostC.txt
 
-
-
 START /wait gzip -d hostB.txt.gz
-REM gzip -d hostC.txt.gz
 
 copy hostA.txt+hostB.txt+hostC.txt+personaliplist.txt host0.txt
 far host0.txt host1.txt 0.0.0.0 127.0.0.1
@@ -27,7 +22,7 @@ sort host9.txt | uniq >>host10.txt
 
 copy top+host10.txt HOSTS /B
 
-del *.txt
+del hostA.txt hostB.txt hostC.txt  host0.txt host1.txt host2.txt host3.txt host4.txt host5.txt host6.txt host7.txt host8.txt host9.txt host10.txt
 del *.gz
 cls
 @ECHO off
@@ -54,7 +49,7 @@ CLS
 @echo. 
 @echo.       Ciao %USERNAME%!
 @echo.
-@echo.         YOU NEED TO UPDATE YOUR HOST! 
+@echo.    YOU NEED TO UPDATE YOUR HOST! 
 @echo.
 @echo.    Please confirm the next admin ask 
 @echo.    for upgrade this file:
